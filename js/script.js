96597292726259190b04fdcb4c74f85250308521
Dmitriy-Rassol <52157280+Dmitriy-Rassol@users.noreply.github.com>
2020-11-17 21:47:00 +0300

    Появление модального окна

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
     // eslint-disable-next-line strict
     'use strict';
-    function countTimer(deadline) {
+
+    // Таймер
+    const countTimer = deadline => {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes =  document.querySelector('#timer-minutes'),
             timerSeconds =  document.querySelector('#timer-seconds');
 
-        function getTimeRemeaning() {
+        const getTimeRemeaning = () => {
             const dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
@@ -15,30 +17,90 @@ window.addEventListener('DOMContentLoaded', () => {
                 hours = Math.floor(timeRemaining / 60 / 60);
 
             return { timeRemaining, hours, minutes, seconds };
-        }
+        };
 
-        function modifyZeroDate(inputDate) {
+        const modifyZeroDate = inputDate => {
             if (inputDate >= 0 && inputDate < 10) {
                 return '0' + inputDate;
             }
             return inputDate;
-        }
+        };
 
-        function updateClock() {
+        const updateClock = () => {
             const timer = getTimeRemeaning();
             timerHours.textContent = modifyZeroDate(timer.hours);
             timerMinutes.textContent = modifyZeroDate(timer.minutes);
             timerSeconds.textContent = modifyZeroDate(timer.seconds);
-            const idSetInterval = setInterval(updateClock, 1000);
+            const idSetAnimation = requestAnimationFrame(updateClock, 1000);
             if (timer.timeRemaining <= 0) {
-                clearInterval(idSetInterval);
+                cancelAnimationFrame(idSetAnimation);
                 timerHours.textContent = '00';
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
             }
-            console.log(timer.seconds);
-        }
+        };
         updateClock();
-    }
-    countTimer('17 nov 2020 03:02:01');
+    };
+
+    countTimer('1 jan 2021');
+
+    // меню
+
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menu = document.querySelector('menu'),
+            menuItems = menu.querySelectorAll('ul>li');
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+
+    // popup
+
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
+
+        let popupContentInterval,
+            count = 100;
+        const animationSpeed = 10;
+
+        const popupContentAnimate = () => {
+            popupContentInterval = requestAnimationFrame(popupContentAnimate);
+            count -= animationSpeed;
+            if (count < 0) {
+                cancelAnimationFrame(popupContentInterval);
+            }
+            popupContent.style.marginTop = count + 'px';
+        };
+
+
+        popupBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                popup.style.display = 'block';
+                const width = document.documentElement.clientWidth;
+                if (width >= 768) {
+                    popupContentAnimate();
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+            count = 150;
+        });
+
+
+    };
+
+    togglePopup();
 });
